00369e18dc783f205ab2691095debddc935336da
felipe <felipefaraggi@gmail.com>
2018-08-26 16:02:24 +0200

    starting the bare settings and config + debug toolbar

--- legacy/urls.py
+++ legacy/urls.py
@@ -6,3 +6,13 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
 ]
+
+
+from django.conf import settings
+from django.conf.urls import include, url
+
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns = [
+        url(r'^__debug__/', include(debug_toolbar.urls)),
+    ] + urlpatterns
