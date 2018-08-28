7f6acafae41480b44a40d590e1e438a1ba7732d5
felipe <felipefaraggi@gmail.com>
2018-08-28 11:04:41 +0200

    home page ready + cleaned up static files and assets + settings edits

--- legacy/settings.py
+++ legacy/settings.py
@@ -111,8 +111,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 
 
 INTERNAL_IPS = [
     '127.0.0.1',
 ]
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'upload')
