ee1f265254f2efd85cadc89af12c0f8d9160f703
felipe <felipefaraggi@gmail.com>
2018-08-30 18:19:56 +0200

    fixed redirect problem when posting empty request

--- support/urls.py
+++ support/urls.py
@@ -1,10 +1,11 @@
-from django.contrib import admin
 from django.urls import path
 
 from . import views
 
 
+app_name = 'support'
+
 urlpatterns = [
-    path('thanks_message/', views.MessageView.as_view(), name='message'),
-    path('thanks_email/', views.EmailView.as_view(), name='email'),
+    path('thanks_message/', views.MessageView.as_view(), name='thanks_message'),
+    path('thanks_email/', views.EmailView.as_view(), name='thanks_email'),
 ]
