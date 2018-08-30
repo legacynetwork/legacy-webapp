00bffbf6d6b686c4448e6fe9e57ee8cde5c15342
felipe <felipefaraggi@gmail.com>
2018-08-30 16:47:48 +0200

    capsules models

--- capsules/models.py
+++ capsules/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from users.models import User
 
-# Create your models here.
+
+class Capsule(models.Model):
+    """
+    Capsule model
+    """
+    # state choice for capsule
+    STATE_CHOICES = [
+        ('o', 'offchain'),
+        ('e', 'empty'),
+        ('t', 'transaction-in-process'),
+        ('i', 'intiated')
+    ]
+    user = models.ForeignKey()
+    name = models.CharField(max_length=30)
+    active = models.BooleanField(default=False)
+    state = models.CharField(max_length=1, choices=STATE_CHOICES, default='o')
+    description = models.CharField(max_length=200)
+    image = models.ImageField(upload_to='capsule_covers/', null=True, blank=True)
+
+    assignees = models.ManyToManyField(User)
