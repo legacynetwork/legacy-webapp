e649130aeae3dad9aced4b9420e9f4176de3bd5c
felipe <felipefaraggi@gmail.com>
2018-08-31 10:42:35 +0200

    small settings update

--- legacy/settings.py
+++ legacy/settings.py
@@ -6,12 +6,14 @@ env = environ.Env()
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-SECRET_KEY = env('DJANGO_SECRET_KEY', default='K64lzmCtvla6s3uinB4KTHuXWsWjMonHHJGHFHtNtAZKrCRiGvmOBuLjVPTLBwgD')
+SECRET_KEY = env(
+    'DJANGO_SECRET_KEY',
+    default='K64lzmCtvla6s3uinB4KTHuXWsWjMonHHJGHFHtNtAZKrCRiGvmOBuLjVPTLBwgD'
+    )
 
 DEBUG = env.bool('DJANGO_DEBUG', False)
 
 ALLOWED_HOSTS = ['legacydapp.com']
-# local
 if DEBUG:
     ALLOWED_HOSTS = ['localhost', 'local', '127.0.0.1']
 
@@ -24,13 +26,18 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+]
+THIRD_PARTY_APPS = [
     'phonenumber_field',
+]
+INTERNAL_APPS = [
     'support',
     'users',
-    'memories',
     'plugins',
 ]
-# local
+
+INSTALLED_APPS += THIRD_PARTY_APPS + INTERNAL_APPS
+
 if DEBUG:
     INSTALLED_APPS += [
         'debug_toolbar',
@@ -78,7 +85,6 @@ DATABASES = {
     'default': env.db('DATABASE_URL', default='postgres:///legacy'),
 }
 
-
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
 
@@ -96,7 +102,6 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
-
 
 # Internationalization
 # https://docs.djangoproject.com/en/2.0/topics/i18n/
@@ -123,13 +128,11 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
 
-
 INTERNAL_IPS = [
     '127.0.0.1',
 ]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'upload')
-
 
 AUTH_USER_MODEL = 'users.User'
 LOGIN_REDIRECT_URL = 'home'
