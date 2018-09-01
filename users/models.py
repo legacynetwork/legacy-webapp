f2aea3a9a47663b70dd6edbe2fc5cbe7f936340b
felipe <felipefaraggi@gmail.com>
2018-09-01 17:53:07 +0200

    user profile ready

--- users/models.py
+++ users/models.py
@@ -1,19 +1,19 @@
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
 from django.contrib.auth.models import User
-
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 from phonenumber_field.modelfields import PhoneNumberField
 
 
-class LegacyUser(models.Model):
+class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
 
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
 
-    first_name = models.CharField(_('first name'), max_length=30, blank=True)
-    last_name = models.CharField(_('last name'), max_length=30, blank=True)
+    name = models.CharField(_('first name'), max_length=255, blank=True)
     birth_date = models.DateTimeField(_('first name'), blank=True, null=True)
     death_date = models.DateTimeField(_('last name'), blank=True, null=True)
 
@@ -28,3 +28,14 @@ class LegacyUser(models.Model):
     ssn = models.CharField(max_length=50, blank=True, null=True)
 
     avatar = models.ImageField(upload_to='avatars/', null=True, blank=True)
+
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
